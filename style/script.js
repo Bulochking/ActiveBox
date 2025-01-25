d26d4a16048eea1d43be6dc6a9f6e9ac7efdf6a4
Bulochking <alexpodstraha@gmail.com>
2025-01-26 00:21:08 +0300

    fix: burger-button -> cross animation

--- style/script.js
+++ style/script.js
@@ -2,7 +2,8 @@ const banner = document.querySelector(".banner");
 const menuButton = document.querySelector(".header__open-menu");
 
 function toggleMenu() {
-    banner.classList.toggle("menu-open"); 	
+    banner.classList.toggle("menu-open");
+    menuButton.classList.toggle("active");	
 }
 
 menuButton.addEventListener("click", toggleMenu);
